9f117ae50e60e42557056fc27a978c02fc207fd7
Salomão Marcioliver <salomaomarcioliver@gmail.com>
2021-09-30 11:47:28 -0300

    0.2v

--- controllers/tabuada.js
+++ controllers/tabuada.js
@@ -1,10 +1,24 @@
 const list = (req, res) => {
-    res.render('tabuada/list')
+    const numeros = []
+    for (let i = 1; i <= 20; i++) {
+        numeros.push(i)
+    }
+    res.render('tabuada/list', { numeros })
 }
 
 const tabuada = (req, res) => {
+    const tabuadas = []
+    const num = req.params.num
+    for(let i=0; i<= 20; i++){
+        tabuadas.push({
+            num,
+            i,
+            resultado: num * i
+        })
+    }
     res.render('tabuada/tabuada', {
-        num: req.params.num
+        num: req.params.num,
+        tabuadas
     })
 }
 
